a8532c8d7b08ef4dbd376ec23722e0f624aa1e3e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-30 12:02:36 +0300

    Added a method that acts as a server

--- BlazorServerProject/Hubs/ChatHub.cs
+++ BlazorServerProject/Hubs/ChatHub.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
 namespace BlazorServerProject.Hubs;
 
-public class ChatHub
+public class ChatHub : Hub
 {
-    
+    public Task SendMesage(string user, string message)
+    {
+        return Clients.All.SendAsync(method:"ReceiveMessage",user, message);
+    }
 }
